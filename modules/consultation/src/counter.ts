1e61d837e4a4ec54e058f3abb82966b1ff5c5eb0
Yonghyun Lee <lyhlg0201@gmail.com>
2024-05-13 22:27:55 +0900

    feat: test

--- modules/consultation/src/counter.ts
+++ modules/consultation/src/counter.ts
@@ -2,7 +2,7 @@ export function setupCounter(element: HTMLButtonElement) {
   let counter = 0
   const setCounter = (count: number) => {
     counter = count
-    element.innerHTML = `count is ${counter}1`
+    element.innerHTML = `count is ${counter}2`
   }
   element.addEventListener('click', () => setCounter(counter + 1))
   setCounter(0)
